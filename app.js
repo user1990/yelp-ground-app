3548ab21d86521cb1a5cf4c54e0d049939258f47
user1990 <paulius.rimg1990@gmail.com>
2017-06-09 12:58:02 +0300

    Created Passport Authentication

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const MongoStore = require('connect-mongo')(session);
@@ -10,9 +12,12 @@ const MongoStore = require('connect-mongo')(session);
 const secret = require('./config/secret');
 const Gym = require('./models/gym');
 const Comment = require('./models/comment');
+const User = require('./models/user');
 const seedDb = require('./seeds');
 
+// ==========
 // APP CONFIG
+// ==========
 mongoose.Promise = global.Promise;
 mongoose.connect(secret.database, (err) => {
   if (err) {
@@ -35,6 +40,24 @@ app.use(session({
 }));
 seedDb();
 
+// PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'Time will tell',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 // ==============
 // RESTFUL ROUTES
 // ==============
@@ -44,6 +67,7 @@ app.get('/', (req, res) => {
 
 // INDEX - show all gyms
 app.get('/gyms', (req, res) => {
+  // Get all gyms from DB
   Gym.find({}, (err, allGyms) => {
     if (err) {
       console.log(err);
@@ -91,7 +115,7 @@ app.get('/gyms/:id', (req, res) => {
 // COMMENTS ROUTES
 // ====================
 
-app.get('/gyms/:id/comments/new', (req, res) => {
+app.get('/gyms/:id/comments/new', isLoggedIn, (req, res) => {
   // find gym by id
   Gym.findById(req.params.id, (err, gym) => {
     if (err) {
@@ -123,6 +147,54 @@ app.post('/gyms/:id/comments', (req, res) => {
   // create new comment
   // connect new comment to gym
 });
+
+// ===========
+// AUTH ROUTES
+// ===========
+
+// Show register form
+app.get('/register', (req, res) => {
+  res.render('accounts/register');
+});
+
+// Handle sign up logic
+app.post('/register', (req, res) => {
+  let newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, (err, user) => {
+    if (err) {
+      console.log(err);
+      return res.render('accounts/register');
+    }
+    passport.authenticate('local')(req, res, function() {
+      res.redirect('/gyms');
+    });
+  });
+});
+
+// Show login form
+app.get('/login', (req, res) => {
+  res.render('accounts/login');
+});
+
+// Handling login logic
+app.post('/login', passport.authenticate('local',
+  {
+    successRedirect: '/gyms',
+    failureRedirect: '/login'
+  }), (req, res) => {});
+
+// Logout logic
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/gyms');
+});
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 // LISTEN TO PORT
 app.listen(secret.port, (err) => {
---
+++ models/user.js
@@ -0,0 +1,13 @@
+'use strict';
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+let UserSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', UserSchema);
+
