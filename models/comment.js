e76797775f0a8c1f69e6e707934784335e50a616
user1990 <paulius.rimg1990@gmail.com>
2017-06-14 14:10:24 +0300

    # Updated schema with cost, location, lat, lng, createdAt

--- models/comment.js
+++ models/comment.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 let commentSchema = mongoose.Schema({
   text: String,
+  createdAt: { type: Date, default: Date.now },
   author: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
--- models/gym.js
+++ models/gym.js
@@ -6,6 +6,11 @@ let gymSchema = new mongoose.Schema({
   name: String,
   image: String,
   description: String,
+  cost: Number,
+  location: String,
+  lat: Number,
+  lng: Number,
+  createdAt: { type: Date, default: Date.now },
   author: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
