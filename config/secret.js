bc9a6677d1ecc4a9f342fff6e7e6f1079ddf4888
user1990 <paulius.rimg1990@gmail.com>
2017-06-15 11:24:08 +0300

    Port

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
   database: 'mongodb://user1990:test123@ds115752.mlab.com:15752/yelpgym',
-  port: process.env.PORT || 3000,
+  port: parseInt(process.env.PORT, 10) || 3000,
   secretKey: 'user1990'
 };
